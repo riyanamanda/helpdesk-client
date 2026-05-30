dc844b4f581048fb58ee018c032be469118920bb
Riyan Amanda <ryant.n92@gmail.com>
2026-05-31 01:05:03 +0700

    feat: add update password a user

--- src/features/user/components/UserActions.tsx
+++ src/features/user/components/UserActions.tsx
@@ -3,33 +3,55 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ROUTES } from "@/constants";
-import { MoreHorizontalIcon, PencilIcon } from "lucide-react";
+import { EditIcon, KeyRoundIcon, MoreHorizontalIcon } from "lucide-react";
+import { useState } from "react";
 import { NavLink } from "react-router";
 import type { User } from "../types";
+import { UpdateUserPasswordDialog } from "./UpdateUserPasswordDialog";
 
 interface UserActionsProps {
     user: User;
 }
 
 export function UserActions({ user }: UserActionsProps) {
+    const [passwordOpen, setPasswordOpen] = useState(false);
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <MoreHorizontalIcon />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem asChild>
-                    <NavLink to={ROUTES.USER.EDIT.replace(":id", user.id)}>
-                        <PencilIcon className="size-3.5" />
-                        Edit
-                    </NavLink>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                        <MoreHorizontalIcon />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                    className="w-(--radix-dropdown-menu-trigger-width) min-w-44 rounded-lg"
+                    align="end"
+                    sideOffset={4}
+                >
+                    <DropdownMenuItem asChild>
+                        <NavLink to={ROUTES.USER.EDIT.replace(":id", user.id)}>
+                            <EditIcon />
+                            Edit
+                        </NavLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onSelect={() => setPasswordOpen(true)}>
+                        <KeyRoundIcon />
+                        Change Password
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <UpdateUserPasswordDialog
+                user={user}
+                open={passwordOpen}
+                onOpenChange={setPasswordOpen}
+            />
+        </>
     );
 }
