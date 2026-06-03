2d601f7749c68407a886ee06bfb539de7ac54248
Riyan Amanda <ryant.n92@gmail.com>
2026-06-03 18:01:51 +0700

    fix: build error because typescript

--- src/i18n/i18next.d.ts
+++ src/i18n/i18next.d.ts
@@ -1,24 +1,7 @@
-import type enAuth from "./locales/en/auth.json";
-import type enCategory from "./locales/en/category.json";
-import type enCommon from "./locales/en/common.json";
-import type enDashboard from "./locales/en/dashboard.json";
-import type enDivision from "./locales/en/division.json";
-import type enHome from "./locales/en/home.json";
-import type enTicket from "./locales/en/ticket.json";
-import type enUser from "./locales/en/user.json";
+export {};
 
 declare module "i18next" {
     interface CustomTypeOptions {
         defaultNS: "common";
-        resources: {
-            common: typeof enCommon;
-            auth: typeof enAuth;
-            ticket: typeof enTicket;
-            category: typeof enCategory;
-            division: typeof enDivision;
-            user: typeof enUser;
-            dashboard: typeof enDashboard;
-            home: typeof enHome;
-        };
     }
 }
